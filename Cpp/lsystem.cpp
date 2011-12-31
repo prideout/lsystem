32c2b9108a942be5291504f1c1d9b74ab8e504de
Philip Rideout <prideout@gmail.com>
2011-12-31 11:43:52 -0800

    repair build

--- Cpp/lsystem.cpp
+++ Cpp/lsystem.cpp
@@ -7,9 +7,12 @@ namespace diag = Diagnostic;
 LSystem::XformList LSystem::Evaluate(const char* filename, int seed)
 {
     pugi::xml_document doc;
+    diag::Print("Reading %s...\n", filename);
     pugi::xml_parse_result result = doc.load_file(filename);
     diag::Check(result, "unable to read XML file %s: %s\n", filename, result.description());
     
+    diag::Print("Evaluating L-System...\n");
+
     XformList retval;
     return retval;
 }
