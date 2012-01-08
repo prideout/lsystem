26e56a1ebf5c2892942d7bc7b9c968250b4c6ea0
Philip Rideout <prideout@gmail.com>
2012-01-08 14:38:35 -0800

    progress on integrating rman

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <lsystem.hpp>
+#include <ri.h>
+
+void _DrawScene()
+{
+}
+
+void _ReportProgress()
+{
+}
 
 int main()
 {
     lsystem ribbon;
     ribbon.Evaluate("Ribbon.xml");
     std::cout << "Success!\n";
+
+    RtContextHandle ri = RiGetContext();
+    RiBegin("launch:prman? -t -ctrl $ctrlin $ctrlout");
+    RiOption("statistics", "endofframe", 1);
+    RiOption("statistics", "xmlfilename", "stats.xml");
+    _DrawScene();
+    _ReportProgress();
+    RiEnd();
 }
