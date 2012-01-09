69e474c29d31229bb1484525b8bff09fb276c668
Philip Rideout <prideout@gmail.com>
2012-01-08 19:35:50 -0800

    still not crashing

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -4,8 +4,34 @@
 
 #pragma GCC diagnostic ignored "-Wwrite-strings" 
 
-void _DrawScene()
+void _DrawWorld()
 {
+    RiWorldBegin();
+    /*
+    RiDeclare("displaychannels", "string");
+    RiDeclare("samples", "float");
+    RiDeclare("coordsys", "string");
+    RiDeclare("hitgroup", "string");
+    RiAttribute("cull", "int backfacing", RI_FALSE);
+    RiAttribute("cull", "int hidden", RI_FALSE);
+    RiAttribute("dice", "int rasterorient", RI_FALSE);
+    RiAttribute("visibility", "int diffuse", RI_TRUE);
+    RiAttribute("visibility", "int specular", RI_TRUE);
+    */
+    {
+      RtToken tokens[1];
+      RtPointer values[1];
+      RtColor em = {0.0/255.0,165.0/255.0,211.0/255.0};
+      tokens[0] = "color em";
+      values[0] = (RtPointer) em;
+      RiSurfaceV("ComputeOcclusion", 1, tokens, values);
+    }
+
+    RiTransformBegin();
+    RiRotate(90, 1, 0, 0);
+    RiDisk(-0.7, 300, 360, RI_NULL);
+    RiTransformEnd();
+    RiWorldEnd();
 }
 
 void _ReportProgress()
@@ -19,35 +45,20 @@ int main()
     std::cout << "Success!\n";
 
     RtContextHandle ri = RiGetContext();
-    RiBegin("launch:prman? -t -ctrl $ctrlin $ctrlout");
-    RiOption("statistics", "endofframe", 1);
-    RiOption("statistics", "xmlfilename", "stats.xml");
-    RiDisplay("grasshopper", "framebuffer", "rgba");
-    RiDisplayChannel("float _occlusion");
+    RiBegin(0); // "launch:prman? -t -ctrl $ctrlin $ctrlout");
+    //RiOption("statistics", "endofframe", 1, RI_NULL);
+    //    RiOption("statistics", "xmlfilename", "stats.xml");
+    RiDisplay("grasshopper", "framebuffer", "rgba", RI_NULL);
+    //RiDisplayChannel("float _occlusion");
     RiShadingRate(8);
     RiPixelSamples(4, 4);
-
-      /*
-    RiProjection(ri.PERSPECTIVE, {ri.FOV: 30})
-    RiTranslate(-1, -1, 20)
-    RiRotate(-20, 1, 0, 0)
-    RiRotate(180, 1, 0, 0)
-    curve = [1, 1, 0.8, 0.1, 0.9, 0.2, 1, 1, 1, 1]
-    RiCamera("world", {"float[10] shutteropening": curve})
-    RiImager("Vignette")
-    RiWorldBegin()
-    RiDeclare("displaychannels", "string")
-    RiDeclare("samples", "float")
-    RiDeclare("coordsys", "string")
-    RiDeclare("hitgroup", "string")
-    RiAttribute("cull", dict(hidden=0,backfacing=0))
-    RiAttribute("dice", dict(rasterorient=0))
-    RiAttribute("visibility", dict(diffuse=1,specular=1))
-*/
-    _DrawScene();
-
-    RiWorldEnd();
-
+    float fov = 30.0f;
+    RiProjection("perspective", RI_FOV, &fov, RI_NULL);
+    RiTranslate(-1, -1, 20);
+    RiRotate(-20, 1, 0, 0);
+    RiRotate(180, 1, 0, 0);
+    RiImager("Vignette", RI_NULL);
+    _DrawWorld();
     _ReportProgress();
     RiEnd();
 }
