d6a314a79fd8ff5efb621fad5ffbee2223bd94b7
Philip Rideout <prideout@gmail.com>
2012-01-22 15:03:38 -0800

    updated to bleeding edge of the go libraries

--- Go/lsystem.go
+++ Go/lsystem.go
@@ -3,29 +3,31 @@ package lsystem
 import (
     "os"
     "fmt"
-    "xml"
+    "encoding/xml"
     )
     
 type LSystem struct {
     MaxDepth int
-    Rules []Rule `xml:"rule>"`
+    Rules []Rule `xml:"rule"`
 }
 
 type Rule struct {
-    Calls []Call `xml:"call>"`
-    Instances []Instance `xml:"instance>"`
-    MaxDepth int `xml:"attr>max_depth"`
-    Successor string `xml:"attr>successor"`
+    Name string  `xml:"name,attr"`
+    Calls []Call `xml:"call"`
+    Instances []Instance `xml:"instance"`
+    MaxDepth int `xml:"max_depth,attr"`
+    Successor string `xml:"successor,attr"`
 }
 
 type Call struct {
-    Transforms string `xml:"attr>transforms"`
-    Rule string  `xml:"attr>rule"`
+    Transforms string `xml:"transforms,attr"`
+    Rule string  `xml:"rule,attr"`
+    Count int   `xml:"count,attr"`
 }
 
 type Instance struct {
-    Transforms string `xml:"transforms>"`
-    Shape string  `xml:"string>"`
+    Transforms string `xml:"transforms"`
+    Shape string  `xml:"string"`
 }
 
 // evaluates the rules in the given XML file and returns a list of curves
@@ -42,6 +44,14 @@ func Evaluate(filename string) int {
     if err = xml.Unmarshal(xmlFile, &lsys); err != nil {
 		fmt.Println("Error parsing XML file:", err)
 		return 0
+	}
+	
+	for r, rule := range lsys.Rules {
+        fmt.Printf("Rule %d. %s -> %s\n", r, rule.Name, rule.Successor)
+	    for i, call := range rule.Calls {
+	        fmt.Printf("%d. count=%d\ttransforms=%s\trule=%s\n", 
+	            i, call.Count, call.Transforms, call.Rule)
+	    }
 	}
 	
     return len(lsys.Rules)
