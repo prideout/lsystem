197c8ebee2b12f0799fe2a1e9a484ab35ea96ef6
Philip Rideout <prideout@gmail.com>
2012-01-22 08:18:55 -0800

    torus demo is complete

---
+++ Go/Occlusion.sl
@@ -0,0 +1,15 @@
+class Occlusion(float darkness = 1;
+                color em = (1,0,1);
+                float samples = 128;
+                float maxdist = 100;
+)
+{
+  public void surface(output color Ci, Oi)
+  {
+    normal Nf = normalize(faceforward(N, I));
+    normal Nn = normalize(Nf); f
+    float occ = darkness * occlusion(P, Nn, samples, "maxdist", maxdist);
+    Ci = (1 - occ) * Cs * em * Os;
+    Oi = Os;
+  }
+}
--- Go/gorman.go
+++ Go/gorman.go
@@ -8,89 +8,6 @@ import (
 	"fmt"
 )
 
-func DrawCurve() {
-	C.curve(0.1, 0.04)
-}
-
-type rtTokens []C.RtToken
-type rtPointers []C.RtPointer
-type rawPointers []unsafe.Pointer
-
-func freeArgs(names rtTokens, owned rawPointers) {
-	for _, v := range names {
-		C.free(unsafe.Pointer(v))
-	}
-	for _, v := range owned {
-		C.free(v)
-	}
-}
-
-func safeArgs(names rtTokens, values rtPointers) (*C.RtToken, *C.RtPointer) {
-	if len(names) == 0 {
-		return new(C.RtToken), new(C.RtPointer)
-	}
-	return &names[0], &values[0]
-}
-
-func unzipArgs(varargs ...interface{}) (names rtTokens, vals rtPointers, owned rawPointers) {
-
-	if len(varargs)%2 != 0 {
-		fmt.Println("odd number of arguments")
-		return
-	}
-
-	nargs := len(varargs) / 2
-	names = make(rtTokens, nargs)
-	vals = make(rtPointers, nargs)
-	owned = make(rawPointers, nargs)
-
-	var pname string
-	for i, v := range varargs {
-
-		// Even-numbered arguments are parameter names
-		if i%2 == 0 {
-			if stringified, ok := v.(string); ok {
-				pname = stringified
-				token := C.CString(pname)
-				names[i/2] = token
-			} else {
-				fmt.Printf("argument %d is not a string\n", i)
-				return
-			}
-			continue
-		}
-
-		// Odd-numbered arguments are values
-		switch v.(type) {
-		case bool:
-			boolified := v.(bool)
-			var intified int32 = 0
-			if boolified {
-				intified = 1
-			}
-			vals[i/2] = C.RtPointer(&intified)
-		case int, int32:
-			intified := v.(int)
-			vals[i/2] = C.RtPointer(&intified)
-		case float32:
-			floatified := v.(float32)
-			vals[i/2] = C.RtPointer(&floatified)
-		case float64:
-			floatified := float32(v.(float64))
-			vals[i/2] = C.RtPointer(&floatified)
-		case string:
-			stringified := v.(string)
-			token := C.CString(stringified)
-			vals[i/2] = C.RtPointer(&token)
-			owned[i/2] = unsafe.Pointer(token)
-		default:
-			fmt.Printf("'%s' has unknown type\n", pname)
-			return
-		}
-	}
-	return
-}
-
 func Display(name string, dtype string, mode string, varargs ...interface{}) {
 	pName := C.CString(name)
 	defer C.free(unsafe.Pointer(pName))
@@ -103,6 +20,42 @@ func Display(name string, dtype string, mode string, varargs ...interface{}) {
 	nargs := C.RtInt(len(varargs) / 2)
 	pNames, pVals := safeArgs(names, values)
 	C.RiDisplayV(pName, pDtype, pMode, nargs, pNames, pVals)
+}
+
+func Disk(height float32, radius float32, tmax float32, varargs ...interface{}) {
+	h := C.RtFloat(height)
+	r := C.RtFloat(radius)
+	t := C.RtFloat(tmax)
+	names, values, ownership := unzipArgs(varargs...)
+	defer freeArgs(names, ownership)
+	nargs := C.RtInt(len(varargs) / 2)
+	pNames, pVals := safeArgs(names, values)
+	C.RiDiskV(h, r, t, nargs, pNames, pVals)
+}
+
+func Torus(majrad float32, minrad float32,
+phimin float32, phimax float32,
+tmax float32, varargs ...interface{}) {
+	a := C.RtFloat(majrad)
+	i := C.RtFloat(minrad)
+	p := C.RtFloat(phimin)
+	x := C.RtFloat(phimax)
+	t := C.RtFloat(tmax)
+	names, values, ownership := unzipArgs(varargs...)
+	defer freeArgs(names, ownership)
+	nargs := C.RtInt(len(varargs) / 2)
+	pNames, pVals := safeArgs(names, values)
+	C.RiTorusV(a, i, p, x, t, nargs, pNames, pVals)
+}
+
+func Attribute(name string, varargs ...interface{}) {
+	pName := C.CString(name)
+	defer C.free(unsafe.Pointer(pName))
+	names, values, ownership := unzipArgs(varargs...)
+	defer freeArgs(names, ownership)
+	nargs := C.RtInt(len(varargs) / 2)
+	pNames, pVals := safeArgs(names, values)
+	C.RiAttributeV(pName, nargs, pNames, pVals)
 }
 
 func Option(name string, varargs ...interface{}) {
@@ -170,6 +123,34 @@ func Begin(name string) {
 	C.RiBegin(pName)
 }
 
+func Declare(name string, decl string) {
+	pName := C.CString(name)
+	defer C.free(unsafe.Pointer(pName))
+	pDecl := C.CString(decl)
+	defer C.free(unsafe.Pointer(pDecl))
+	C.RiDeclare(pName, pDecl)
+}
+
+func End() {
+	C.RiEnd()
+}
+
+func WorldBegin() {
+	C.RiWorldBegin()
+}
+
+func WorldEnd() {
+	C.RiWorldEnd()
+}
+
+func TransformBegin() {
+	C.RiTransformBegin()
+}
+
+func TransformEnd() {
+	C.RiTransformEnd()
+}
+
 func ShadingRate(val float32) {
 	C.RiShadingRate(C.RtFloat(val))
 }
@@ -186,10 +167,90 @@ func Rotate(angle, x, y, z float32) {
 	C.RiRotate(C.RtFloat(angle), C.RtFloat(x), C.RtFloat(y), C.RtFloat(z))
 }
 
-func End() {
-	C.RiEnd()
+// Private utility functions for managing name-value pairs \\
+
+type rtTokens []C.RtToken
+type rtPointers []C.RtPointer
+type rawPointers []unsafe.Pointer
+
+func freeArgs(names rtTokens, owned rawPointers) {
+	for _, v := range names {
+		C.free(unsafe.Pointer(v))
+	}
+	for _, v := range owned {
+		C.free(v)
+	}
 }
 
-func Temp() {
-	C.curve2(0.1, 0.04)
+func safeArgs(names rtTokens, values rtPointers) (*C.RtToken, *C.RtPointer) {
+	if len(names) == 0 {
+		return new(C.RtToken), new(C.RtPointer)
+	}
+	return &names[0], &values[0]
+}
+
+func unzipArgs(varargs ...interface{}) (names rtTokens,
+vals rtPointers, owned rawPointers) {
+
+	if len(varargs)%2 != 0 {
+		fmt.Println("odd number of arguments")
+		return
+	}
+
+	nargs := len(varargs) / 2
+	names = make(rtTokens, nargs)
+	vals = make(rtPointers, nargs)
+	owned = make(rawPointers, nargs)
+
+	var pname string
+	for i, v := range varargs {
+
+		// Even-numbered arguments are parameter names
+		if i%2 == 0 {
+			if stringified, ok := v.(string); ok {
+				pname = stringified
+				token := C.CString(pname)
+				names[i/2] = token
+			} else {
+				fmt.Printf("argument %d is not a string\n", i)
+				return
+			}
+			continue
+		}
+
+		// Odd-numbered arguments are values
+		switch v.(type) {
+		case bool:
+			boolified := v.(bool)
+			var intified int32 = 0
+			if boolified {
+				intified = 1
+			}
+			vals[i/2] = C.RtPointer(&intified)
+		case int, int32:
+			intified := v.(int)
+			vals[i/2] = C.RtPointer(&intified)
+		case [3]float32:
+			floatified := v.([3]float32)
+			vals[i/2] = C.RtPointer(&floatified[0])
+		case [4]float32:
+			floatified := v.([4]float32)
+			vals[i/2] = C.RtPointer(&floatified[0])
+		case float32:
+			floatified := v.(float32)
+			vals[i/2] = C.RtPointer(&floatified)
+		case float64:
+			floatified := float32(v.(float64))
+			vals[i/2] = C.RtPointer(&floatified)
+		case string:
+			stringified := v.(string)
+			token := C.CString(stringified)
+			vals[i/2] = C.RtPointer(&token)
+			owned[i/2] = unsafe.Pointer(token)
+		default:
+			fmt.Printf("'%s' has unknown type\n", pname)
+			return
+		}
+	}
+	return
 }
