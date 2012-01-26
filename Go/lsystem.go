2c2149360417008486cbcfd3d7dfc154c48e4d6b
Philip Rideout <prideout@gmail.com>
2012-01-25 22:31:59 -0800

    progress

--- Go/lsystem.go
+++ Go/lsystem.go
@@ -49,11 +49,12 @@ func Evaluate(stream io.Reader) Curve {
     }
 
     random := rand.New(rand.NewSource(42))
-    var stackNode StackNode
-    stackNode.RuleIndex = lsys.PickRule("entry", random)
-    stackNode.Transform = vmath.M4Identity()
+    start := StackNode {
+        RuleIndex: lsys.PickRule("entry", random),
+        Transform: vmath.M4Identity(),
+    }
 
-    lsys.ProcessRule(stackNode, &curve, random)
+    lsys.ProcessRule(start, &curve, random)
     return curve
 }
 
@@ -83,7 +84,55 @@ type Instance struct {
     Shape      string `xml:"string"`
 }
 
-func (self *LSystem) ProcessRule(stackNode StackNode, curve *Curve, random *rand.Rand) {
+func radians(degrees float32) float32 {
+    return degrees * 3.1415926535 / 180.0
+}
+
+// pkg/container/list might make more sense; just an interesting exercise!
+
+type StackNode struct {
+    RuleIndex int
+    Depth     int
+    Transform *vmath.M4
+}
+
+type Stack struct {
+    s []StackNode 
+}
+
+func (self *Stack) Clone() *Stack {
+    x := new(Stack)
+    x.s = make([]StackNode, len(self.s))
+    copy(x.s, self.s)
+    return x
+}
+
+func (self *Stack) Push(stackNode StackNode) {
+    self.s = append(self.s, stackNode)
+}
+
+func (self *Stack) Pop() StackNode {
+    var e StackNode
+    n := len(self.s)-1
+    e, self.s = self.s[n], self.s[:n]
+    return e
+}
+
+// if we were to use pass-by-value for 'self', it would be more const-like
+// but, would it be less efficient?
+func (self *Stack) Len() int {
+    return len(self.s)
+}
+
+func (self *LSystem) ProcessRule(start StackNode, curve *Curve, random *rand.Rand) {
+    
+    stack := new(Stack)
+    stack.Push(start)
+    
+    for stack.Len() > 0 {
+        e := stack.Pop()
+        fmt.Print(e)
+    }
 }
 
 func (self *LSystem) PickRule(name string, random *rand.Rand) int {
@@ -175,10 +224,4 @@ func (cache *MatrixCache) ParseString(s string) {
     }
     (*cache)[s] = *xform
     return
-}
-
-type StackNode struct {
-    RuleIndex int
-    Depth     int
-    Transform *vmath.M4
 }
